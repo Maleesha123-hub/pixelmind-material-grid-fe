988c1e285fe72442e274b125a8f8b857976e72bc
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2026-08-22 21:50:50 +0530

    change the port

--- vite.config.mjs
+++ vite.config.mjs
@@ -27,7 +27,7 @@ export default defineConfig(() => {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
     },
     server: {
-      port: 3000,
+      port: 8080,
       allowedHosts: ['*'],
       proxy: {
         // https://vitejs.dev/config/server-options.html
